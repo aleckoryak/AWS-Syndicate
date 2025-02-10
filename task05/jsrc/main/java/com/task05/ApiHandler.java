5f54afb69b40b00901b5a3307cff2c665dbec72c
Oleksiy_Koryak <Oleksiy_Koryak@epam.com>
2025-02-09 21:12:17 -0500

    task5 rerrite input parser

--- task05/jsrc/main/java/com/task05/ApiHandler.java
+++ task05/jsrc/main/java/com/task05/ApiHandler.java
@@ -2,6 +2,7 @@ package com.task05;
 
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.document.Item;
 import com.amazonaws.services.dynamodbv2.model.AttributeValue;
 import com.amazonaws.services.dynamodbv2.model.PutItemRequest;
 import com.amazonaws.services.dynamodbv2.model.PutItemResult;
@@ -12,6 +13,8 @@ import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPEvent;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPResponse;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.syndicate.deployment.annotations.environment.EnvironmentVariable;
+import com.syndicate.deployment.annotations.environment.EnvironmentVariables;
 import com.syndicate.deployment.annotations.lambda.LambdaHandler;
 import com.syndicate.deployment.annotations.lambda.LambdaUrlConfig;
 import com.syndicate.deployment.annotations.resources.DependsOn;
@@ -23,6 +26,7 @@ import com.syndicate.deployment.model.lambda.url.InvokeMode;
 
 import java.time.Instant;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.UUID;
 
@@ -38,34 +42,43 @@ import java.util.UUID;
         invokeMode = InvokeMode.BUFFERED
 )
 @DependsOn(name = "Events", resourceType = ResourceType.DYNAMODB_TABLE)
-public class ApiHandler implements RequestHandler<APIGatewayV2HTTPEvent, APIGatewayV2HTTPResponse> {
+@EnvironmentVariables(value = {
+        @EnvironmentVariable(key = "region", value = "${region}"),
+        @EnvironmentVariable(key = "table", value = "${target_table}")})
+public class ApiHandler implements RequestHandler<Object, APIGatewayV2HTTPResponse> {
     private static final ObjectMapper objectMapper = new ObjectMapper();
     private static final AmazonDynamoDB dynamoDB = AmazonDynamoDBClientBuilder.defaultClient();
 
     @Override
-    public APIGatewayV2HTTPResponse handleRequest(APIGatewayV2HTTPEvent event, Context context) {
+    public APIGatewayV2HTTPResponse handleRequest(Object event, Context context) {
         LambdaLogger logger = context.getLogger();
         try {
             logger.log("Received event: " + event.toString());
-            // Parse input from APIGatewayV2HTTPEvent
-            Map<String, Object> input = objectMapper.readValue(event.getBody(), Map.class);
-            Integer principalId = (Integer) input.get("principalId");
-            Map<String, Object> content = (Map<String, Object>) input.get("content");
+            logger.log("Lambda logger EVENT: " + objectMapper.writeValueAsString(event));
+            logger.log("EVENT TYPE: " + event.getClass());
+            // Parse input from APIGatewayV2
+            Map<String, Object> input = objectMapper.readValue(objectMapper.writeValueAsString(event), LinkedHashMap.class);
 
-            // Generate UUID and timestamps
+            // Parse input
+            Integer principalId = (Integer) input.get("principalId");
+            Map<String, String> content = (Map<String, String>) input.get("content");
+
+            // Generate UUID
             String eventId = UUID.randomUUID().toString();
             String createdAt = Instant.now().toString();
 
-            // DynamoDB item
+            // Prepare item for DynamoDB
             Map<String, AttributeValue> item = new HashMap<>();
             item.put("id", new AttributeValue(eventId));
             item.put("principalId", new AttributeValue().withN(principalId.toString()));
             item.put("createdAt", new AttributeValue(createdAt));
             item.put("body", new AttributeValue(objectMapper.writeValueAsString(content)));
 
-            // Save to DynamoDB
-            PutItemRequest putItemRequest = new PutItemRequest().withTableName("Events").withItem(item);
+            // Save item to DynamoDB
+            PutItemRequest putItemRequest = new PutItemRequest().withTableName(System.getenv("table")).withItem(item);
             PutItemResult putItemResult = dynamoDB.putItem(putItemRequest);
+
+            logger.log("putItemResult:" + putItemResult.toString());
 
             // Prepare response
             Map<String, Object> responseBody = new HashMap<>();
